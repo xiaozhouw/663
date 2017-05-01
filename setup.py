b0ae624cd27ccc5b09c09e07031de717e9fe3eac
xiaozhouw <xiaozhou.wang@duke.edu>
2017-05-01 01:32:41 -0400

    Create setup.py
    setup1

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='hmm',
+    version='1.0.0',
+
+    description='Implementation of the Linear Sparse Version Algorithms to Hidden Markov Model',
+    long_description= This is an implementation of the memory sparse version of the Baum-welch and Viterbi algorithms to HMM.,
+    url='https://github.com/xiaozhouw/663',
+    author='Hao Sheng, Xiaozhou Wang',
+    author_email='hao.sheng@duke.edu,xiaozhou.wang@duke.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='hidden markov model',
+    install_requires=['numpy','numba'],
+)
